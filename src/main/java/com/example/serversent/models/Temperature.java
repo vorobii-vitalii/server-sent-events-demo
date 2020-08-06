3e06c0ed76514d13a380a155f43f8885d522ea09
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-08-06 14:32:24 +0300

    Added Temperature model

---
+++ src/main/java/com/example/serversent/models/Temperature.java
@@ -0,0 +1,13 @@
+package com.example.serversent.models;
+
+public class Temperature {
+    private final double value;
+
+    public Temperature(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
+}
