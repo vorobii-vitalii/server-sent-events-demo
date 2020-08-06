dca83621fb0d8796b3bd943cfe3c1f9c44f493b6
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-08-06 14:38:11 +0300

    Added ServerSentApplication

---
+++ src/main/java/com/example/serversent/ServerSentApplication.java
@@ -0,0 +1,13 @@
+package com.example.serversent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSentApplication.class, args);
+	}
+
+}
